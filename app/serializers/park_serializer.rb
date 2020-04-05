7820cd277e065932913dd940821899b12df9a86e
Zach Weber <weberzt@gmail.com>
2020-04-05 18:30:24 -0500

    removed photo upload from user model

--- app/serializers/park_serializer.rb
+++ app/serializers/park_serializer.rb
@@ -2,5 +2,5 @@ class ParkSerializer < ActiveModel::Serializer
   attributes :id, :name, :state, :description, :weather, :longitude, :latitude, :image, :creator_id
   has_many :tags, include_nested_attributes: :true
   # has_many :users, include_nested_attributes: :true
-  # has_many :reviews, include_nested_attributes: :true
+  has_many :reviews, include_nested_attributes: :true
 end
