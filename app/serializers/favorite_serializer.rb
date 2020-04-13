8481f49617240b94801a001f3f719783354d1449
Zach Weber <weberzt@gmail.com>
2020-04-13 18:11:20 -0500

    fixed favorites serializer

--- app/serializers/favorite_serializer.rb
+++ app/serializers/favorite_serializer.rb
@@ -1,5 +1,5 @@
 class FavoriteSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :park_id
+  attributes :id, :user_id, :park_id, :user, :park
   belongs_to :user, include_nested_attributes: :false
   belongs_to :park, include_nested_attributes: :false
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,5 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :username, :email, :password, :image, :created_at
   has_many :reviews, include_nested_attributes: :true
   has_many :parks, include_nested_attributes: :true
+  has_many :favorites, include_nested_attributes: :true
 end
